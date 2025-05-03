7b0590f2e93bcc5f886b875c1fe73b91c7877a2a
OrichalcosShunnoros <c3146092091@gmail.com>
2025-05-03 15:45:26 -0500

    añadidos archivos para posterior añadidura del contenido de datos

---
+++ src/data/gibeom.ts
@@ -0,0 +1,98 @@
+/*
+BASICOS DE CUADRADO
+
+BLANCO
+1. IL CHASE SAJOE CHUCHUM SOGUI
+    Montong An SonNal Chirugui, Batang-Son Chirugui.
+
+AMARILLO
+2. I CHASE SAJOE CHUCHUM SOGUI
+    Montong Bakkat SonNal Makko, Agum-Son Chirugui.
+
+NARANJA
+3. SAM CHASE SAJOE CHUCHUM SOGUI
+    Are SonNal Makko, Pyon-Son Kub Chirugui.
+
+ROJO
+4. SA CHASE SAJOE AP KUBI SON MOK
+    Ap Kubi Are Son Mok, Pyojeok Bakkata Dari Chagui. Ap Kubi Pyojeok Pal
+    Kup Chigui, Dung Chumok Gigui. (16 movimientos).
+
+ROJO MARRÓN
+5. O' CHASE, AP KUBI, ME CHUMOK OLIGUI
+      Desplaza al lado, desde Mo Chuchum Sogui, Me Chumok Oligui, en el mismo
+      puesto cambia a Ap Kubi. Montong Pan Chumok Chirigui. Chaba, Ap Chagui
+      Neryo Batang Makki, Dip Kubi, Pyojok Neryo Me Chumok Bom Sogui Bende
+      Olgul Makki, Parop Olgul Makki, Chuchum Sogui Iop Chumok Chigui. (24  
+      movimientos)
+
+BÁSICOS DE GEUMGANG
+
+NARANJA
+1. CHUCHUM SOGUI, ANDARI SOGUI, AP KUBI SOGUI.
+    Chuchum Sogui Montong Chumok Chirugui. Iop Andari Sogui, Wesanteul Makki, 
+    Ap Kubi, Baro Montong Chumok Chirugui, Pal Kup Chiruigui. Grito.
+
+VERDE
+2. KUN GAM CHUMOK 1
+    Ap Dip Kubi KunGam Iop Chumok Chirugui, Ap Kubi Bande Montong Chumok  
+    Chirugui, Ap Chagui (kihap). Dip Dip Kubi Sogui, Montong Son Nal Makki.
+
+3. KUN GAM CHUMOK 2
+    Ap Dip Kubi, KunGam Iop Chumok Chirugui, An Son Nal Chigo, Bakkat Son Nal     
+    Chigui Ap Kubi Bande Montong Chumok Chigui, Ap Chagui (Kihap). Dip Dip Kubi 
+    Sogui, Montong Son Nal Koduro Makki.
+
+AZUL
+4. KUN GAM CHUMOK 3
+    Ap Dip Kubi, Kun Gam Son Nal Makki, Are Son Nal Koduro Makki, Huryo Chagui, grito. 
+    Ap Ap Kubi Olgul Makki, Montong Chumok Chigui, Sin Avanzar Dip Kubi 
+    Montong Son Nal Koduro Makki.
+
+
+5. KUN GAM CHUMOK 4
+    Ap Dip Kubi, Kun Gam Son Nal Makki, Are Son Nal Koduro Makki, al frente patea Ap Chagi y regresa a Dip 
+    Kubi. Anya Huryo Chagui. Grito. Terminando el giro en Dip Kubi, Kun Gam Son 
+    Nal Makki, con el pie de atrás Anya Dolyo Chagi y regresa a Dip Kubi Montong 
+    Koduro Son Nal Makki girando sobre el pie atrasado.
+
+
+-BÁSICOS DE RESPIRACIÓN 
+
+BÁSICO DE RESPIRACIÓN 1
+
+BLANCO
+1.	JeomBe. Inhala desde Moa Sogi, eleva los brazos en Kyop Son JeomBe Sogi, baja los brazos por los costados a la vez que exhala. Inhala de nuevo, separa la pierna izquierda para quedar en Chuchum Sogi, Montong Tang Jong Dul Batang Son, Montong Okkoro Makki, vuelve las manos a la cintura en DoJeumokeori Sogi y de nuevo, Montong Dul Batang Son Chigi.
+2.	Al lado izquierdo las dos manos la de abajo empuñada y la de arriba abierta con los dedos juntos, golpea Uen Montong Batang Son Chigi, recoge al otro lado, la mano izquierda queda por encima y abierta y la de abajo empuñada, golpea Orum Montong Batang Son Chigi.
+3.	Hace Uen Bakkat SonNal Makki, con la derecha, un giro y Deung Jeumok en la palma de la mano izquierda, y hace esto por el lado derecho (repite 2 veces por cada lado)
+4.	Después del Deung Jeumok desliza la mano derecha por el antebrazo izquierdo hasta llegar al codo, realiza un giro hacia afuera y golpea Uen Montong Deung Jeumok, abre la mano hacia la derecha y golpea Pal Kub Chigi a la mano abierta, realiza un giro y golpea de nuevo Montong Deung Jeumok (repite este movimiento 2 veces por cada lado)
+5.	Hace una ‘’X’’ con los antebrazos y realiza Dul Yop Son Mok Chigi simultáneamente a cada lado lentamente con la respiración (repite este movimiento 4 veces.)
+6.	Sube el brazo izquierdo y baja el derecho, quedando en Uen Olgul Son Mok Chigi, Orum Are Batang Makki, invierte con la respiración lenta, sube derecha y baja izquierda con los mismos dos movimientos (repite este movimiento 4 veces sin contar los anteriores) 
+
+AMARILLO.
+7.	Recoge en Koap Sogi al lado derecho, y realiza Uen Montong Bakkat SonNal Makki Tang Jong, pasa a Chuchum, Sogi Orum Montong Jeumok Chigi. (repite este movimiento 3 veces más cambiando de lado) terminando con Orum Montong Bakkat Makki Tang Jong.
+8.	Pasa a Chuchum Sogi, golpea Orum Montong Pyon Son Gi a la vez que la palma izquierda cure el codo derecho por encima, hace lo mismo por el otro lado (repite este movimiento una vez más por cada lado)
+9.	Inhala desde Moa Sogi, eleva los brazos en Kyop Son Chumbi Sogi, baja los brazos por los costados a la vez que exhala. Inhala de nuevo, avanza un paso largo al frente y cae en Koap Sogi Dul Montong Pyon-Son Gi Chigi (Grito).
+
+NARANJA.
+10.	Abre al lado izquierdo Ap Kubi, con la mano izquierda marca la zona genital al frente y ejecuta Are Ap Son-Mok Chigi a la mano abierta, realiza un círculo hacia atrás con ambas manos abiertas a la vez que se ejecuta Orum Mureup Chagi (repite el mismo movimiento al lado derecho y repite una vez más por cada lado) terminando al lado derecho en Uen Handari Sogi con las manos por encima de la rodia y apuntando hacia afuera.
+11.	Gira al frente, cae en ChuChum Sogi y realiza Orum Chumok Chirigui a la zona genital, Uen Montong Chumok Chirigui en la zona del ombligo, Montong Chumok Chirigui en la zona del plexo y Uen Olgul Chumok Chirigui a la cara.
+12.	Recoge a Moap Sogui, respira como al principio. Chumbe.
+
+BASICO DE RESPIRACIÓN 2
+
+AZUL
+1.	Chumbe. Inhala desde Moap Sogui, eleva los brazos en Kyopson Chumbi Sogui, baja los brazos por los costados a la vez que exhala. Inhala de nuevo, separa la pierna izquierda para quedar en Chuchum Sogui, Are Tang Jong, Olgul Tang Jong (arriba abajo, abajo arriba) Cuatro veces.
+2.	En Ap Kubi, Neryo Son Nal Chigui, Pyojeok Son Nal Neeryo Son Nal Chigui la mano izquierda.
+3.	En Ap Kubi, Kawi Batang Makki (empieza mano derecha arriba), An Tary Chanoki, cae atrás en Dip Kubi y Montong Son Mal Bakkat Makki (patea primero la pierna derecha) (4 movimientos).
+4.	Recoge la pierna izquierda en y en Chuchum Sogui, Son Nal Are Jecho Makki, Iop Chanoki (empieza izquierda), cae en Ap Chuchum Sogui y Son Nal Weasanteul Makki, recoge de nuevo a Chuchum Sogui Son Nal Are Jecho Makki y repite por el otro lado. (4 movimientos).
+5.	4 Montong Batang Son Chigui (empeza mano izquierda).
+6.	Mon Dolyo Dip Kubi, Son Nal Are Koduro Makki (Bloqueo primero mano izquierda) (4 movimientos) 
+
+PURPURA
+7.	Recoge la pierna izquierda a Dwichuk Moap Sogui, respira como al inicio Kyopson Chumbi Sogui a la vez que bloquea Montong Okkoru Makki. Hace Chun Ban Nak Bop, y desde ahí rollo lateral , golpea Wen Batang Son Chigui, Dung Chumok Chigui. Grito. (mano contraria al pie adelantado).
+8.	Desde la posición anterior hace rollo lateral al lado izquierdo y por el lado izquierdo (termina pierna izquierda adelante) a la vez que golpea Orum Batang Son Chigui, Dung Chumok Chigui. (mano contraria al pie adelantado), Sin levantarse da media vuelta en la misma posición con las manos con Montong Koduro Son Nal Makki como guardia (mano acompañante empuñada). Realiza el mismo movimiento por el otro lado. (4 movimientos)
+9.	Se levanta por la espalda y queda en Ap Kubi, a la vez que hace inhalación arriba (con las manos a la altura del pecho y las palmas hacia arriba), sigue al frente, exhala estirando los brazos completamente en Dul Pal Batang Son Chigui, Luego Dul Pal An Son Nal Chigui rápido, Cargándolo desde atrás y con un leve zapateo. Recoge al otro lado. (4 movimientos).
+10.	Gira por la espalda ¼ de vuelta en Ap Kubi, batang son Montong Bakkat Nullo Makki, Dul Uon Chumok Chigui, Agum-Son Chigui (abdomen, plexo, garganta). Avanza en koap Sogui (derecha por delante) (acompaña cerrada). Grito. Se desenrosca el Koap Sogui para quedar en Chuchum Sogui con Are Tang Jong. Recoge a Moap Sogui, respira como al principio. Chumbe.
+
+*/
---
+++ src/data/mugiGibeom.ts
@@ -0,0 +1,141 @@
+/*
+
+CON NUNCHAKU SENCILLO
+
+BLANCO
+
+1.  Medio círculo hacia al frente y hacia arriba y cambio de mano.
+2.  Vuelta por dentro y cambio de mano.
+3.  Vuelta por dentro, por fuera y cambio de mano.
+4.  Cambios por debajo del hombro y hacia arriba.
+5.  Golpe abajo al lado contrario, al otro lado, sube y cambia de mano.
+6.  3 giros hacia el frente bajando y 3 giros hacia atrás subiendo y cambia de mano.
+7.  Igual que 4+3 sube y cambia de mano.
+8.  Igual que 5 reemplazando el último golpe con un giro por fuera hacia atrás y cambia de mano.
+9.  Inicia como 5 y envía el Nunchaku por entre las piernas, sube como 1 y cambia de mano.
+10. Golpes hacia adentro y a la cabeza cambiando de mano a lado y lado.
+
+AMARILLO
+
+11. Desde la axila giro al frente y por dentro.
+12. Igual que 11 cambiando de mano iniciando con mano cruzada.
+13. Igual que 11 pero el giro por fuera.
+14. Igual que 13 cambiando de mano iniciando con mano cruzada.
+15. Igual que 11 pero el giro por dentro y por fuera.
+16. Igual que 15 cambiando de mano iniciando con mano cruzada.
+17. Igual que 15 pero un lado.
+18. Igual que 17 cambiando de mano iniciando con mano cruzada.
+19. Igual que 18 pero cambia de lado pasando el Nunchaku por el cuello y desenvuelve terminando el 18.
+20. Igual que 4 invertido.
+
+NARANJA
+
+21. Igual que 15 devuelve con el filo de la otra mano.
+22. Igual que 17 a un lado, sigue el giro, pasa por detrás terminado con 3 a un lado.
+23. Igual que 4 por detrás.
+24. Vuelta por encima y 5.
+25. Igual 24 cambiando el último golpe por un giro horizontal hacia afuera seguido de un giro vertical hacia el frente (para abajo).
+
+VERDER
+
+26. Pasa el Nunchaku por la muñeca hacia atrás varias veces.
+27. Giro por dentro, giro por fuera pasando por la mano por fuera y luego por dentro varias veces.
+28. Igual que 24, pasa el Nunchaku por debajo como 26 y sube con 27 y 3.
+29. Igual que 28 pero con doble 27.
+30. Equis por lado y lado pasando por encima de la mano igual que 26.
+
+AZUL
+
+31. Igual que 26 abajo con un giro posterior.
+32. Igual que 31 pero pasando de mano.
+33. Igual que31 pero en el medio.
+34. Igual que 19 pero con cambios como 31.
+35. Igual que 27 pero frenando el último giro quedando vertical, devuelve y vuelve a empezar.
+
+PÚRPURA
+
+36. Igual que 27 pero devuelve con la mano contraria como 21 y sube con 27 cambia de mano y vuelve a empezar.
+37. 1ra parte del primer callejón de I Chol Bong I Dan, solo el 24 terminando al otro lado.
+38. 2da parte del primer callejón de I Chol Bong I Dan.
+39. 3ra parte del primer callejón de I Chol Bong I Dan como el anterior pero pasando por detrás para iniciar 
+    por el otro lado.
+40. Are Bakkat Makki con el Nunchaku sube y cambia de lado, ejecuta 5 a la vez que hace un giro por el frente 
+    para quedar en el otro lado y vuelve a empezar.
+
+
+CON NUNCHAKU DOBLE
+
+
+ROJO
+
+1.	1 (sim)
+2.	2 (sim)
+3.	3 (sim)
+4.	Hombro, axila (sim)
+5.	4 trocada.
+6.	6 (sim)
+7.	7 desde abajo.
+8.	7, 15 desde axilas (sim)
+9.	7, 15, 13 (sim)
+10.	11 (sim)
+
+ROJO MARRÓN
+
+11.	13 (sim)
+12.	15 (sim)
+13.	17 (sim)
+14.	5 (alt)
+15.	8 (alt)
+16.	9 (alt) vuelta adentro afuera y arriba.
+17.	Medio 24 (alt)
+18.	25 (alt)
+19.	10 (alt)
+20.	5 (sim)
+
+MARRÓN
+
+21.	8 (sim)
+22.	9 (sim)
+23.	10 (sim)
+24.	24 (sim)
+25.	25 (sim)
+26.	X adelante (sim)
+27.	X atrás (sim)
+28.	26 (sim)
+29.	27 (sim)
+30.	Disociado (17/15)
+
+
+CON JANG BONG (PALO LARGO)
+AZUL
+
+1.	Recoge desde el piso con el pie.
+2.	Descarga con el pie.
+3.	Desliza entre las manos a lado y lado.
+4.	Lanza un extremo y coge el otro (una mano) ±
+5.	4 pero cambiando de mano ±
+6.	Remo ±
+7.	Igual que 6 caminando de frente y de espaldas ±
+8.	Igual que 6 girando ±
+9.	Ventilador ±
+10.	Remo con una mano ±
+
+PÚRPURA
+
+11.	10 cambiando de mano con 9 ±
+12.	Helicóptero ±
+13.	Igual que 12 y pasa girándolo por la espalda ±
+14.	Igual que 13 pero arrodillándose y levantándose ±
+15.	Igual que 13 pero solo arrodillado ±
+16.	Giros adentro y afuera sostenido de la punta ±
+17.	Igual que 16 pero con las dos manos ±
+18.	Giros igual que 16 pero terminando golpeando el piso.
+19.	Envaina y desenvaine.
+20.	Lanza por el cuello igual que 19 de Nunchacos, y recibe al otro lado.
+
+ROJO
+
+21.	Punzón como lanza alta.
+22.	Giro por la espalda y punzón de lanza.
+
+*/
